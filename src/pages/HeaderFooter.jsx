d5589edbe40d57d8aa99e1ffd2336810fd23c424
Fawwaz RN <fawwaz1511@student.abudzar.sch.id>
2026-03-03 10:55:10 +0700

    logo

--- src/pages/HeaderFooter.jsx
+++ src/pages/HeaderFooter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router";
+import logo from "/logo.png";
 
 export default function HeaderFooter() {
   // Helper untuk styling NavLink dengan palet baru
@@ -12,11 +13,16 @@ export default function HeaderFooter() {
     <div className="flex flex-col bg-[#F3F4F4] min-h-screen font-sans">
       {/* Header */}
       <header className="top-0 z-50 fixed border-[#5F9598]/10 border-b w-full">
-        <div className="flex justify-between items-center bg-white/80 backdrop-blur-md px-6 md:px-20 py-4 w-full">
-          <div className="flex items-center gap-2">
-            <h1 className="font-black text-[#061E29] text-2xl tracking-tighter">
-              Edu<span className="text-[#1D546D]">Course</span>
-            </h1>
+        <div className="flex justify-between items-center bg-white/90 backdrop-blur-md px-6 md:px-20 py-3 w-full">
+          {/* Logo Section */}
+          <div className="flex items-center">
+            <NavLink to="/">
+              <img
+                src={logo}
+                alt="EduCourse Logo"
+                className="w-auto h-10 md:h-12 object-contain"
+              />
+            </NavLink>
           </div>
 
           <nav className="flex items-center space-x-8 font-medium text-sm uppercase tracking-wide">
@@ -57,9 +63,7 @@ export default function HeaderFooter() {
       {/* Footer */}
       <footer className="bg-[#061E29] px-6 md:px-20 pt-12 pb-6 text-[#F3F4F4]">
         <div className="flex md:flex-row flex-col justify-between items-center mb-8 pb-8 border-[#1D546D]/30 border-b">
-          <h2 className="mb-4 md:mb-0 font-bold text-[#5F9598] text-xl uppercase tracking-widest">
-            EduCourse
-          </h2>
+          <img src="" alt="" />
           <div className="flex gap-6 text-[#5F9598] text-sm">
             <a href="#" className="hover:text-white transition-colors">
               Instagram
